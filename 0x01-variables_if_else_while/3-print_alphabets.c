25ace8baafd7017e966b602f68313e4143cf5ad6
Denver-Baraka <denverbaraka@gmail.com>
2022-12-09 12:58:42 -0800

    upper and lower

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main -Entry point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char alphabet[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int i = 0;
+
+	while (i < 52)
+	{
+		putchar (alphabet[i]);
+		if (i == 25)
+		{
+			putchar('\n');
+		}
+		i++;
+	}
+	putchar ('\n');
+
+	return (0);
+}
