a8bd94776a5b28355ba9910e1e2d51e82e8863bc
Denver-Baraka <denverbaraka@gmail.com>
2022-12-09 14:24:43 -0800

    hex

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main -Entry point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char hex[] = "0123456789abcdef";
+	int i = 0;
+
+	while (i < 16)
+	{
+		putchar (hex[i]);
+		i++;
+	}
+	putchar ('\n');
+
+	return (0);
+}
