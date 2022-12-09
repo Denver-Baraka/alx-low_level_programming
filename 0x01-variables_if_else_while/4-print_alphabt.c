258e49b7cde202283a20fc7a555959cd6e4bc97a
Denver-Baraka <denverbaraka@gmail.com>
2022-12-09 13:36:18 -0800

    ommitt q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main -Entry point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char alphabet[] = "abcdefghijklmnopqrstuvwxyz";
+	int i = 0;
+	int qascii = 113;
+	int eascii = 101;
+	int l;
+
+	while (i < 26)
+	{
+		l = alphabet[i];
+		if (l == qascii || l == eascii)
+		{
+		}
+		else
+		{
+		putchar (alphabet[i]);
+		i++;
+		}
+	}
+	putchar ('\n');
+
+	return (0);
+}
