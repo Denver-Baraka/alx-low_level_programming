0edc1d2abd2a1bedf13924332a15bd94fe6a95c9
Denver-Baraka <denverbaraka@gmail.com>
2022-12-14 09:34:22 -0800

    print all lowercase alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,10 +1,17 @@
-#include "1-main.c"
+int _putchar(char c);
+
+/**
+ * print_alphabet prints all lower alphabet letters
+ * @abcdefghijklmopqrst letters to print
+ * Return: 0 success
+ */
+
 void print_alphabet(void)
 {
-	char alphabet[]="abcdefghijklmnopqrstuvwxyz";
-	int i=0;
+	char alphabet[] = "abcdefghijklmnopqrstuvwxyz";
+	int i = 0;
 
-	while(i < 26)
+	while (i < 26)
 	{
 		_putchar(alphabet[i]);
 		i++;
