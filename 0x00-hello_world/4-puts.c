55c23bcc48315619e5c5b8970e11fb75ac20816f
Denver-Baraka <denverbaraka@gmail.com>
2023-07-13 20:00:33 +0300

    4. Hello with puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 
+/**
+ * main - starting point of the program
+ * @parameterx: no parameters, void
+ *
+ * just a main function
+ * Return: 0 success
+ */
+
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
