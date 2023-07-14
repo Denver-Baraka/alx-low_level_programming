2f9cd38d9351d23be634c9b597b8c325383d5d3b
Denver-Baraka <denverbaraka@gmail.com>
2023-07-14 21:04:56 +0300

    4. Alphabet no q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - starting point
+ *
+ * Return: success => 0
+ */
+
+int main(void)
+{
+	int i = 0;
+	char alphabet[] = "abcdefghijklmnopqrstuvwxyz";
+
+	while (alphabet[i] != '\0')
+	{
+		if (alphabet[i] == 'q' || alphabet[i] == 'e')
+		{
+		}
+		else
+			putchar(alphabet[i]);
+		i++;
+	}
+	putchar('\n');
+
+	return (0);
+}
