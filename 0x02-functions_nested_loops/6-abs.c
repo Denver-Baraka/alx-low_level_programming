ff47fc8e1e967aaf7f9c3f423ddd2e7d6361c8d8
Denver-Baraka <denverbaraka@gmail.com>
2023-07-19 14:22:37 +0300

    6. absolute

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,15 +1,24 @@
 /**
  * _abs - returns absolute value of a number
  * @n: number whose absolute value is needed
- * 
+ *
  * Return:
  *	absolute(n)
  */
 
 int _abs(int n)
 {
-	if (n < 0)
+	if (n == -2147483648)
+	{
+		n = n + 1;
 		return (n * (-1));
+	}
+	else if (n < 0)
+	{
+		return (n * (-1));
+	}
 	else
-		return n;
+	{
+		return (n);
+	}
 }
