d84acfd1c2cec4ab87d9306f16146cd21b0e9fae
Denver-Baraka <denverbaraka@gmail.com>
2023-07-19 09:30:10 +0300

    1. print alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,10 +1,17 @@
 #include "main.h"
 
+/**
+ * print_alphabet - print all alphabet letters in lower case
+ *
+ * Example:
+ *	print_alphabet() --> abcdefghijklmnopqrstuvwxyz
+ */
+
 void print_alphabet(void)
 {
 	int i = 'a';
 
-	while(i <= 'z')
+	while (i <= 'z')
 	{
 		_putchar(i);
 		i++;
