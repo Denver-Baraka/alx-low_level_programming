cf5a01b392c743d3604fc173f84ccc18eb1078a6
Denver-Baraka <denverbaraka@gmail.com>
2023-07-19 14:09:24 +0300

    7. fix for INT_MIN

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,15 +10,13 @@
 int print_last_digit(int n)
 {
 	int last_digit;
-	int number;
-
-	if (n < 0)
-		number = n * -1;
-	else
-		number = n;
+	long number = (long)n;
+	
+	if (number < 0)
+		number = number * -1;
 
 	last_digit = number % 10;
-
 	_putchar('0' + last_digit);
+
 	return (last_digit);
 }
