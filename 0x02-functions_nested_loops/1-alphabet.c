44d0537ecd948f85806247278a58c6a2961b9833
Denver-Baraka <denverbaraka@gmail.com>
2023-07-19 09:21:04 +0300

    1. print alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+void print_alphabet(void)
+{
+	int i = 'a';
+
+	while(i <= 'z')
+	{
+		_putchar(i);
+		i++;
+	}
+	_putchar('\n');
+}
---
+++ 0x02-functions_nested_loops/1-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - starting point
+ *
+ * Return: Success => 0
+ */
+
+int main(void)
+{
+	print_alphabet();
+
+	return (0);
+}
