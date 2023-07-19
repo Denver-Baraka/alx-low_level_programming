f450a9cc7b53e8f83ae5f11aa431c8456b2a8291
Denver-Baraka <denverbaraka@gmail.com>
2023-07-19 10:10:02 +0300

    2. all functions now in one file

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - prints alphabet 10 times
+ * print_alphabet_x10 - executes print_alphabet() 10 times
  *
  * Description:
  *	will print all alphabet letters in lower case
@@ -18,4 +18,24 @@ void print_alphabet_x10(void)
 		print_alphabet();
 		i++;
 	}
+}
+
+/**
+ * print_alphabet - prints all letters of the alphabet
+ *
+ * Description:
+ *	lowercase and followed by newline
+ *	abcdefghijklmonpqrstuvwxyz
+ */
+
+void print_alphabet(void)
+{
+	int i = 'a';
+
+	while (i <= 'z')
+	{
+		_putchar(i);
+		i++;
+	}
+	_putchar('\n');
 }
