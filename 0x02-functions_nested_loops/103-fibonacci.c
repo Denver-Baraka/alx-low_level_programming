d8aa016cb34133464d88a21860b61a1df2fa9e87
Denver-Baraka <denverbaraka@gmail.com>
2023-07-28 22:00:59 +0300

    0x02 NC 16

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - starting point
+ *
+ * Return: 0 --> sucess
+ */
+int main(void)
+{
+	int i = 0;
+	long a = 2, b = 4;
+	long fibo, checker;
+
+	printf("%ld, %ld, ", a, b);
+
+	while (checker < (4000000 - fibo - b - a))
+	{
+		fibo = a + b;
+		a = b;
+		b = fibo;
+		printf("%ld, ", fibo);
+	}
+	fibo = a + b;
+	printf("%ld\n", fibo);
+
+	return (0);
+}
