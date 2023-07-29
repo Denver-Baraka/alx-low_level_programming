84b1760b21da7e2ab48a6a6ec713dc2790971b10
Denver-Baraka <denverbaraka@gmail.com>
2023-07-29 16:37:09 +0300

    try

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+int rev(int i, int j);
+/**
+ * main - start
+ *
+ * Return: 0 --> success
+ */
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (i != j && rev(i, j))
+			{
+				putchar('0' + i);
+				putchar('0' + j);
+				if (i == 8 && j == 9)
+					putchar('\n');
+				else
+					putchar(',');
+			}
+		}
+	}
+	return (0);
+}
+/**
+ * rev - checks if a number has been previously printed
+ * @i: number
+ * @j: number remember it putchar
+ *
+ * Return: if true --> 0 else 1
+ */
+int rev(int i, int j)
+{
+	int prev = (j * 10) + i;
+	int current = (i * 10) + j;
+
+	if (current > prev)
+		return (0);
+	return (1);
+}
