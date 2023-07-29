22a461d2b7ce5832ed3fc267e82f68e66783763d
Denver-Baraka <denverbaraka@gmail.com>
2023-07-29 10:18:19 +0300

    try 98 fibo

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - starting point
+ *
+ * Return: 0 --> sucess
+ */
+int main(void)
+{
+	int i = 0;
+	long a = 1, b = 2;
+	long fibo = 0, sum = 2;
+
+	printf("%ld, %ld, ", a, b);
+
+	while (i < 96)
+	{
+		fibo = a + b;
+		a = b;
+		b = fibo;
+		printf("%ld, ", fibo);
+		i++;
+	}
+	fibo = a + b;
+	printf("%ld\n", fibo);
+	return (0);
+}
