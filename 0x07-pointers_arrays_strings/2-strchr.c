12b8f56b2ed36ed44b9be2c01e3d9ce84187cc4d
Denver-Baraka <denverbaraka@gmail.com>
2023-07-31 19:22:06 +0300

    0x07 2. strchr try 1

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+/**
+ * _strchr - locates a character in a string
+ * @s: a string
+ * @c: the character
+ *
+ * Return: pointer to the 1st ocurrence of c in s
+ *	   else --> returns NULL
+ */
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	while (*(s + i) != '\0')
+	{
+		if (c == *(s + i))
+			return (s + i);
+		i++;
+	}
+
+	return ("0");
+}
