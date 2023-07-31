e43e1db616d4a1c35fa2431c1b5240ca484bbcaf
Denver-Baraka <denverbaraka@gmail.com>
2023-07-31 21:24:46 +0300

    try

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -9,3 +9,6 @@ int is_subset(char c, char *set);
 unsigned int _strspn(char *s, char *accept);
 /* 4. strpbrk */
 char *_strpbrk(char *s, char *accept);
+/* 6. chess */
+void print_chessboard(char (*a)[8]);
+int _putchar(char c);
