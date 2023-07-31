544e3daddea1c757878352289b61bd6878931af2
Denver-Baraka <denverbaraka@gmail.com>
2023-07-31 20:37:19 +0300

    0x07 3. strnspn try 1

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,4 +1,11 @@
 #include "main.h"
+/**
+ * _strspn - gets lenght of a prefix substring
+ * @s: string to be analyzed
+ * @accept: string containing characters to match
+ *
+ * Return: length of the initial segment of str
+ */
 unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int i = 0;
@@ -9,8 +16,15 @@ unsigned int _strspn(char *s, char *accept)
 			return (i);
 		i++;
 	}
-	return (i + 1);
+	return (i);
 }
+/**
+ * is_subset - checks if a character is contained in string
+ * @c: the character
+ * @set: the string set
+ *
+ * Return: is contained --> 1, else --> 0
+ */
 int is_subset(char c, char *set)
 {
 	int i = 0;
